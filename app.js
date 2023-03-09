0aea18162137e86e649ad2603a86a28b35af353a
JP <85654561+jp524@users.noreply.github.com>
2023-03-08 21:00:12 -0500

    Add book to library in console

---
+++ app.js
@@ -0,0 +1,29 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = Boolean(read);
+}
+
+Book.prototype.info = function info() {
+  return `${this.title} by ${this.author}, ${this.pages}, ${
+    this.read ? 'read' : 'not read yet'
+  }`;
+};
+
+function addBookToLibrary(title, author, pages, read) {
+  myLibrary.push(new Book(title, author, pages, read));
+}
+
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
+addBookToLibrary(
+  'Harry Potter and the Chamber of Secrets',
+  'J.K. Rowling',
+  305,
+  true
+);
+addBookToLibrary('Twilight', 'Stephanie Meyer', 500, true);
+
+console.log(myLibrary);
